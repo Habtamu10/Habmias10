68b02a7a7a9f63b420fa42d275d58fe27802e0ef
Habtamu Biks <habtamubiks4@gmail.com>
2026-03-15 21:22:58 +0000

    Add Trie implementation

---
+++ algorithms/trie.py
@@ -0,0 +1,40 @@
+# Trie (Prefix Tree) implementation
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+trie = Trie()
+for word in ["apple", "app", "application", "apply"]:
+    trie.insert(word)
+print(trie.search("app"))
+print(trie.search("ap"))
+print(trie.starts_with("appl"))
