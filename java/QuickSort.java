cbf9d177434a70c83df874aedfd925fa75a23180
Habtamu Biks <habtamubiks4@gmail.com>
2026-03-15 21:22:58 +0000

    Add Java quick sort

---
+++ java/QuickSort.java
@@ -0,0 +1,28 @@
+// Quick Sort in Java
+import java.util.Arrays;
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
+            }
+        }
+        int tmp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = tmp;
+        return i + 1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+    }
+}
