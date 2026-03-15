3c7b10a00c5fe0dd40db95ac02b269ee6080cca6
Habtamu Biks <habtamubiks4@gmail.com>
2026-03-15 21:22:58 +0000

    Add JavaScript binary search

---
+++ javascript/binary_search.js
@@ -0,0 +1,13 @@
+// Binary Search in JavaScript
+function binarySearch(arr, target) {
+    let left = 0, right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+const arr = [2, 3, 4, 10, 40];
+console.log("Index:", binarySearch(arr, 10));
